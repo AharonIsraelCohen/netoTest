bf46cdf99259e3e6b51ceba7ec2c0ed987ebbdf5
root <root@netomedia-jenkins.us-central1-a.c.extended-creek-316418.internal>
2021-06-11 12:44:17 +0000

    test

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 echo $1
 
-kubectl patch deployment netomedia-deployment -p'{"spec":{"template":{"spec":{"containers":[{"name": "nodejs", "image":"ac770/netomedia:$1"}]}}}}'
+kubectl patch deployment netomedia-deployment -p'{"spec":{"template":{"spec":{"containers":[{"name": "nodejs", "image":"'ac770/netomedia:"$1"'"}]}}}}'
